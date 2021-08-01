c62d492c967a6ae9c535836e81b5f568f9431716
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2021-08-02 00:59:22 +0900

    learn prop-types

--- udemy-demo-app/src/App.js
+++ udemy-demo-app/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {name: "Noname"}
+    {name: "Noname", age: 3}
   ]
   return(
     <div>
     {/* 下記の{}はJavaScriptをHTML表示させるため */}
     {
       profiles.map((profile, index) => {
-        console.log(index); //profilesにあるデータ数の分表示される mapには必ずあるもので、普段は省略されている
+        {/* console.log(index); //profilesにあるデータ数の分表示される mapには必ずあるもので、普段は省略されている */}
         return <User name={profile.name} age={profile.age} key={index} />
       })
     }    
@@ -23,9 +24,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
 
-//データが入ってこなかった場合、以下で設定したデフォルトデータが自動で挿入される
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired //isRequiredをつけると必ず値が入れる必要がある
 }
 
 
