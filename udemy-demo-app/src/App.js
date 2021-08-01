519e3ffae1fc5fd2fea50928b77505443dfc3b34
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2021-08-02 01:23:09 +0900

    learn state

--- udemy-demo-app/src/App.js
+++ udemy-demo-app/src/App.js
@@ -1,33 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "Noname", age: 3}
-  ]
-  return(
-    <div>
-    {/* 下記の{}はJavaScriptをHTML表示させるため */}
-    {
-      profiles.map((profile, index) => {
-        {/* console.log(index); //profilesにあるデータ数の分表示される mapには必ずあるもので、普段は省略されている */}
-        return <User name={profile.name} age={profile.age} key={index} />
-      })
-    }    
-    </div>
-  ) 
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component{
+  //Componentの初期化時はconstructorを用いる
+  constructor(props){
+    super(props);
+    console.log(this.state);
+    this.state = {count: 0};
+  }
+
+  handlePlusButton = () => {
+    console.log(this.state.count);
+    // const count = this.state.count;
+    // this.setState({count: count + 1})
+
+    // setStateでrenderメソッドが呼び出されて、ここで状態を変更したい処理を記載する
+    this.setState({count: this.state.count + 1}) //上2行分がこの1行に集約
+  }
+
+  handleMinusButton = () => {
+    console.log(this.state.count)
+    this.setState({count: this.state.count - 1}) 
+  }
+
+  render(){
+    console.log("render")
+    return(
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired //isRequiredをつけると必ず値が入れる必要がある
-}
-
 
 export default App;
