260d2976e03f824cab169157d3c52006e0dc45ea
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2021-08-02 00:51:00 +0900

    learn props

--- udemy-demo-app/src/App.js
+++ udemy-demo-app/src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
 
-// ファンクショナルコンポーネント
-// function App() {
-//   return(
-//     <div>
-//         <input type="text" onClick={() => {console.log("Click!")}} />
-//         <label htmlFor="bar" ht="true">bar</label>
-//         <input type="text" onChange={() => {console.log("Change!")}} />
-//     </div>   
-//   )
-// }
-
-// 以下は関数コンポーネントの練習
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "Noname"}
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+    {/* 下記の{}はJavaScriptをHTML表示させるため */}
+    {
+      profiles.map((profile, index) => {
+        console.log(index); //profilesにあるデータ数の分表示される mapには必ずあるもので、普段は省略されている
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }    
     </div>
   ) 
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
 
+//データが入ってこなかった場合、以下で設定したデフォルトデータが自動で挿入される
+User.defaultProps = {
+  age: 1
+}
 
 
 export default App;
