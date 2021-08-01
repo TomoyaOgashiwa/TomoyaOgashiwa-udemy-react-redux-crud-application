30d50b5d45b63f398fc2ac7376ea3c3f61d20409
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2021-08-02 00:12:44 +0900

    learn more JSX

--- udemy-demo-app/src/App.js
+++ udemy-demo-app/src/App.js
@@ -1,23 +1,19 @@
-import { render } from "@testing-library/react";
 import React from "react";
 
-function App() {
-  return (
-    <h1>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello World!
-    </h1>
-  );
-}
-
-// 上記と同じ処理 上記がJSX、下記がJavaScriptで表示したもの
 // function App() {
-//   render();
-//   return React.createElement(
-//     "div",
-//     null,
-//     "Hello World!!!!"
-//   );
+//   const greeting = "Hi Tom!";
+//   const dom = <h1 className="foo">{greeting}</h1>
+//   return dom;
 // }
+
+function App() {
+  return(
+    <div>
+        <input type="text" onClick={() => {console.log("Click!")}} />
+        <label htmlFor="bar" ht="true">bar</label>
+        <input type="text" onChange={() => {console.log("Change!")}} />
+    </div>   
+  )
+}
 
 export default App;
